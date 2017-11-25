606610c85eccea78944e3f280e0a2a6ebdacde2d
xschen <xs0040@gmail.com>
2017-11-25 15:00:11 +0800

    navigation

---
+++ Assets/UnityUtility/NavComp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NavComp : MonoBehaviour
+{
+
+// Use this for initialization
+protected virtual void Start()
+{
+
+}
+
+// Update is called once per frame
+protected virtual void Update()
+{
+
+}
+}
---
+++ Assets/UnityUtility/NavManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NavManager : MonoBehaviour
+{
+
+	private NavComp[] mNavCompCollection;
+	private static NavManager mInstance;
+
+	void Awake()
+	{
+	    mNavCompCollection = FindObjectsOfType<NavComp>();
+
+	    if (mInstance == null)
+	    {
+	        mInstance = this;
+	    }
+	    else
+	    {
+	        Debug.Log("Should not reach here");
+	    }
+	}
+
+	public static NavManager Instance
+	{
+	    get
+	    {
+	        return mInstance;
+	    }
+	}
+
+	public NavComp FindNearestNavComp(Transform transform)
+	{
+	    Vector3 source = transform.position;
+	    float minDistance = float.MaxValue;
+	    NavComp selectedNavComp = null;
+	    for (int i = 0; i < mNavCompCollection.Length; ++i )
+	    {
+	        NavComp nc = mNavCompCollection[i];
+	        Vector3 candidate = nc.gameObject.transform.position;
+	        float dX = source.x - candidate.x;
+	        float dZ = source.z - candidate.z;
+	        //float dY = source.y - candidate.y;
+
+	        float distance = Mathf.Sqrt(dX * dX + dZ * dZ);
+	        if (distance < minDistance)
+	        {
+	            minDistance = distance;
+	            selectedNavComp = nc;
+	        }
+	    }
+
+	    if (selectedNavComp == null)
+	    {
+	        Debug.Log("selectedNavComp == null!!!!");
+	    }
+
+	    return selectedNavComp;
+	}
+
+	public NavComp FindNearestNavCompOtherThan(Transform transform, NavComp other)
+	{
+	    Vector3 source = transform.position;
+	    float minDistance = float.MaxValue;
+	    NavComp selectedNavComp = null;
+	    for (int i = 0; i < mNavCompCollection.Length; ++i)
+	    {   
+	        NavComp nc = mNavCompCollection[i];
+	        if (nc == other) continue;
+
+	        Vector3 candidate = nc.gameObject.transform.position;
+	        float dX = source.x - candidate.x;
+	        float dZ = source.z - candidate.z;
+	        //float dY = source.y - candidate.y;
+
+	        float distance = Mathf.Sqrt(dX * dX + dZ * dZ);
+	        if (distance < minDistance)
+	        {
+	            minDistance = distance;
+	            selectedNavComp = nc;
+	        }
+	    }
+
+	    if (selectedNavComp == null)
+	    {
+	        Debug.Log("selectedNavComp == null!!!!");
+	    }
+
+	    return selectedNavComp;
+	}
+}
